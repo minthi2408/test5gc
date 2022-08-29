77237f5d2cee92157a38b8b899303d8bc1b69d40
Thai Quang Tung <tqtung@t420s>
2022-08-30 00:13:20 +0900

    producers' methods refactoring

--- nfs/amf/sbi/producer/handler.go
+++ nfs/amf/sbi/producer/handler.go
@@ -108,3 +108,122 @@ func MakeTestService() (service httpdp.HttpService) {
 func myapphandler(ctx openapi.RequestContext) *openapi.Response {
 	return nil
 }
+
+func (prod *Producer) makeCommServices(p Backend) (service httpdp.HttpService) {
+    fn := openapi_http.BuildProducerRequestHandler
+	service.Routes = httpdp.HttpRoutes{
+		{
+			"Index",
+			"GET",
+			"/",
+			httpdp.HttpIndexHandler,
+		},
+
+		{
+			"AMFStatusChangeSubscribeModify",
+			strings.ToUpper("Put"),
+			"/subscriptions/:subscriptionId",
+			fn(amfprod.HTTPAMFStatusChangeSubscribeModify, prod.HandleAMFStatusChangeSubscribeModify ),
+		},
+
+		{
+			"AMFStatusChangeUnSubscribe",
+			strings.ToUpper("Delete"),
+			"/subscriptions/:subscriptionId",
+			fn(amfprod.HTTPAMFStatusChangeUnSubscribe, prod.HTTPAMFStatusChangeUnSubscribe),
+		},
+
+		{
+			"CreateUEContext",
+			strings.ToUpper("Put"),
+			"/ue-contexts/:ueContextId",
+			fn(amfprod.HTTPCreateUEContext, prod(HTTPCreateUEContext),
+		},
+
+		{
+			"EBIAssignment",
+			strings.ToUpper("Post"),
+			"/ue-contexts/:ueContextId/assign-ebi",
+			fn(amfprod.HTTPEBIAssignment, prod.HTTPEBIAssignment),
+		},
+
+		{
+			"RegistrationStatusUpdate",
+			strings.ToUpper("Post"),
+			"/ue-contexts/:ueContextId/transfer-update",
+			fn(amfprod.HTTPRegistrationStatusUpdate, prod.HTTPRegistrtionStatusUpdat),
+		},
+
+		{
+			"ReleaseUEContext",
+			strings.ToUpper("Post"),
+			"/ue-contexts/:ueContextId/release",
+			fn(amfprod.HTTPReleaseUEContext, prod.HTTPReleaseUEContext),
+		},
+
+		{
+			"UEContextTransfer",
+			strings.ToUpper("Post"),
+			"/ue-contexts/:ueContextId/transfer",
+			fn(amfprod.HTTPUEContextTransfer, prod.HTTPUEContextTransfer),
+		},
+
+		{
+			"N1N2MessageUnSubscribe",
+			strings.ToUpper("Delete"),
+			"/ue-contexts/:ueContextId/n1-n2-messages/subscriptions/:subscriptionId",
+			h.HTTPN1N2MessageUnSubscribe,
+		},
+
+		{
+			"N1N2MessageTransfer",
+			strings.ToUpper("Post"),
+			"/ue-contexts/:ueContextId/n1-n2-messages",
+			h.HTTPN1N2MessageTransfer,
+		},
+
+		{
+			"N1N2MessageTransferStatus",
+			strings.ToUpper("Get"),
+			"/ue-contexts/:ueContextId/n1-n2-messages/:n1N2MessageId",
+			h.HTTPN1N2MessageTransferStatus,
+		},
+
+		{
+			"N1N2MessageSubscribe",
+			strings.ToUpper("Post"),
+			"/ue-contexts/:ueContextId/n1-n2-messages/subscriptions",
+			fn(amfprod.HTTPN1N2MessageSubscribe, prod.HTTPN1N2MessageSubscribe),
+		},
+
+		{
+			"NonUeN2InfoUnSubscribe",
+			strings.ToUpper("Delete"),
+			"/non-ue-n2-messages/subscriptions/:n2NotifySubscriptionId",
+			h.HTTPNonUeN2InfoUnSubscribe,
+		},
+
+		{
+			"NonUeN2MessageTransfer",
+			strings.ToUpper("Post"),
+			"/non-ue-n2-messages/transfer",
+			h.HTTPNonUeN2MessageTransfer,
+		},
+
+		{
+			"NonUeN2InfoSubscribe",
+			strings.ToUpper("Post"),
+			"/non-ue-n2-messages/subscriptions",
+			h.HTTPNonUeN2InfoSubscribe,
+		},
+
+		{
+			"AMFStatusChangeSubscribe",
+			strings.ToUpper("Post"),
+			"/subscriptions",
+			h.HTTPAMFStatusChangeSubscribe,
+		},
+	}
+	service.Group = SERVICE_NAME
+	return
+}
--- openapi/producers/amf/communication.go
+++ openapi/producers/amf/communication.go
@@ -10,3 +10,38 @@ func HTTPAMFStatusChangeSubscribeModify(ctx openapi.RequestContext) {
 	//decode the request (and body)
 	ctx.DecodeRequest(&subData)
 }
+func HTTPAMFStatusChangeUnSubscribe(ctx openapi.RequestContext) {
+    //do nothing
+}
+
+func HTTPCreateUEContext(ctx openapi.RequestContext) {
+    /*
+    tqtung: complex 
+
+	var createUeContextRequest models.CreateUeContextRequest
+	createUeContextRequest.JsonData = new(models.UeContextCreateData)
+    */
+}
+// EBIAssignment - Namf_Communication EBI Assignment service Operation
+func HTTPEBIAssignment(ctx openapi.RequestContext) {
+	var body models.AssignEbiData
+    ctx.DecodeRequest(&body)
+}
+
+func HTTPRegistrationStatusUpdate(ctx openapi.RequestContext) {
+	var body models.UeRegStatusUpdateReqData
+    ctx.DecodeRequest(&body)
+}
+
+func HTTPReleaseUEContext(ctx openapi.RequestContext) {
+	var body models.UeContextRelease
+    ctx.DecodeRequest(&body)
+}
+
+func (h *Handler) HTTPUEContextTransfer(ctx openapi.RequestContext) {
+    //TODO: complex scenario
+}
+
+func HTTPN1N2MessageUnSubscribe(ctx openapi.ReqeustContext) {
+    //do nothing
+}
