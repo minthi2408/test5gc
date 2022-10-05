83cb849f342c9a143b3efc73acfc631ba29a3107
Thai Quang Tung <anan@2an.etri.re.kr>
2022-10-05 19:31:43 +0900

    format with gofmt

--- nfs/udm/sbi/producer/producer.go
+++ nfs/udm/sbi/producer/producer.go
@@ -7,6 +7,12 @@ import (
 	"etri5gc/nfs/udm/context"
 	sbi_httpdp "etri5gc/sbi/httpdp"
 	udmee "etri5gc/sbi/udm/ee"
+	udmmt "etri5gc/sbi/udm/mt"
+	udmniddau "etri5gc/sbi/udm/niddau"
+	udmpp "etri5gc/sbi/udm/pp"
+	udmsdm "etri5gc/sbi/udm/sdm"
+	udmueau "etri5gc/sbi/udm/ueau"
+	udmuecm "etri5gc/sbi/udm/uecm"
 
 	"github.com/sirupsen/logrus"
 )
@@ -36,10 +42,33 @@ func New(b Backend) *Producer {
 func (prod *Producer) Services() []common.Service {
 	services := make([]common.Service, 7, 7)
 	services[0] = httpdp.HttpService{
-		Group:  "comm",
+		Group:  "ee",
 		Routes: sbi_httpdp.MakeHttpRoutes(udmee.Routes, prod),
 	}
-
+	services[1] = httpdp.HttpService{
+		Group:  "pp",
+		Routes: sbi_httpdp.MakeHttpRoutes(udmpp.Routes, prod),
+	}
+	services[2] = httpdp.HttpService{
+		Group:  "mt",
+		Routes: sbi_httpdp.MakeHttpRoutes(udmmt.Routes, prod),
+	}
+	services[3] = httpdp.HttpService{
+		Group:  "sdm",
+		Routes: sbi_httpdp.MakeHttpRoutes(udmsdm.Routes, prod),
+	}
+	services[4] = httpdp.HttpService{
+		Group:  "ueau",
+		Routes: sbi_httpdp.MakeHttpRoutes(udmueau.Routes, prod),
+	}
+	services[5] = httpdp.HttpService{
+		Group:  "uecm",
+		Routes: sbi_httpdp.MakeHttpRoutes(udmuecm.Routes, prod),
+	}
+	services[6] = httpdp.HttpService{
+		Group:  "niddau",
+		Routes: sbi_httpdp.MakeHttpRoutes(udmniddau.Routes, prod),
+	}
 	return services
 }
 
